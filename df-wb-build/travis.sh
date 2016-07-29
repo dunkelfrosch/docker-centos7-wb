1a57b98c6d303e4d0c7eecf4d47e0609113e7d36
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2016-07-29 13:19:53 +0200

    travis, try to fix container_check subProcess(es)

--- df-wb-build/travis.sh
+++ df-wb-build/travis.sh
@@ -124,10 +124,10 @@ echo "y" | ./dctl --build-all-images
 
 echo -e "\n - check runtime availability for all microServices ... \n"
 _container_check --image-name "local/df/wb/centos/7" --container-name "df-wb-centos7" --container-service-count 17 && \
-_container_check --image-name "local/df/wb/centos/7/httpd/apache" --container-name "df-wb-centos7-httpd" --container-service-count 19 && \
-_container_check --image-name "local/df/wb/centos/7/httpd/apache/php56" --container-name "df-wb-centos7-httpd-php56" --container-service-count 20 && \
-_container_check --image-name "local/df/wb/centos/7/httpd/apache/php70" --container-name "df-wb-centos7-httpd-php70" --container-service-count 20 && \
-_container_check --image-name "local/df/wb/centos/7/rdbs/mysql57" --container-name "df-wb-centos7-rdbs-mysql57" --container-service-count 19 && \
-_container_check --image-name "local/df/wb/centos/7/rdbs/mysql56" --container-name "df-wb-centos7-rdbs-mysql56" --container-service-count 17 && \
-_container_check --image-name "local/df/wb/centos/7/rdbs/postresql95" --container-name "df-wb-centos7-rdbs-pgsql95" --container-service-count 19 && \
-_container_check --image-name "local/df/wb/centos/7/rdbs/postresql93" --container-name "df-wb-centos7-rdbs-pgsql93" --container-service-count 19
+_container_check --image-name "local/df/wb/centos/7/httpd/apache" --container-name "df-wb-centos7-httpd" --container-service-count 18 && \
+_container_check --image-name "local/df/wb/centos/7/httpd/apache/php56" --container-name "df-wb-centos7-httpd-php56" --container-service-count 19 && \
+_container_check --image-name "local/df/wb/centos/7/httpd/apache/php70" --container-name "df-wb-centos7-httpd-php70" --container-service-count 19 && \
+_container_check --image-name "local/df/wb/centos/7/rdbs/mysql57" --container-name "df-wb-centos7-rdbs-mysql57" --container-service-count 18 && \
+_container_check --image-name "local/df/wb/centos/7/rdbs/mysql56" --container-name "df-wb-centos7-rdbs-mysql56" --container-service-count 16 && \
+_container_check --image-name "local/df/wb/centos/7/rdbs/postresql95" --container-name "df-wb-centos7-rdbs-pgsql95" --container-service-count 18 && \
+_container_check --image-name "local/df/wb/centos/7/rdbs/postresql93" --container-name "df-wb-centos7-rdbs-pgsql93" --container-service-count 18
