aa980fa4d597b7f915265d823e1ad8f08286d828
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2016-07-29 12:41:34 +0200

    fix travis build with new docker version including minor refactoring issues inside travis build processor script

--- df-wb-build/travis.sh
+++ df-wb-build/travis.sh
@@ -6,7 +6,7 @@
 #
 # @copyright (c) 2016 Patrick Paechnatz <patrick.paechnatz@gmail.com>
 # @author patrick.paechnatz@gmail.com
-# @updated 2016/27/07
+# @updated 2016/29/07
 # @version 1.0.1
 #
 
@@ -89,11 +89,11 @@ _container_check() {
     echo "wait 3 seconds before service check ..."; sleep 3s
    _cnt=$(docker logs ${cnt_name} | grep '\[*OK' | wc -l)
     if [ $_cnt -eq ${service_count} ]; then
-        echo -e "\033[1;92m[SUCCESS] all required systemd services loaded  -> ${img_name} <${cnt_name}>\033[0m";
+        echo -e "\033[1;92m[SUCCESS] all required systemd services loaded -> ${img_name} <${cnt_name}>\033[0m";
     else
-        echo -e "\n\033[0;31m[FAILURE] not all service loaded successfully!\033[0m <EXIT>\n"
-        echo "          -> looking for ${service_count} services but found ${_cnt}"
-        _docker_container_clear
+        echo -e "\n\033[0;31m[FAILURE] NOT all service loaded successfully! -> ${img_name} <${cnt_name}>\033[0m <EXIT>\n"
+        echo -e "          -> looking for ${service_count} services but found ${_cnt}\n" && docker logs ${cnt_name}
+       _docker_container_clear
         exit 9
     fi
 
@@ -123,11 +123,11 @@ echo -e "\n - build all images necessary for this workbench ... \n"
 echo "y" | ./dctl --build-all-images
 
 echo -e "\n - check runtime availability for all microServices ... \n"
-_container_check --image-name "local/df/wb/centos/7" --container-name "df-wb-centos7" --container-service-count 18
-_container_check --image-name "local/df/wb/centos/7/httpd/apache" --container-name "df-wb-centos7-httpd" --container-service-count 19
-_container_check --image-name "local/df/wb/centos/7/httpd/apache/php56" --container-name "df-wb-centos7-httpd-php56" --container-service-count 20
-_container_check --image-name "local/df/wb/centos/7/httpd/apache/php70" --container-name "df-wb-centos7-httpd-php70" --container-service-count 20
-_container_check --image-name "local/df/wb/centos/7/rdbs/mysql57" --container-name "df-wb-centos7-rdbs-mysql57" --container-service-count 19
-_container_check --image-name "local/df/wb/centos/7/rdbs/mysql56" --container-name "df-wb-centos7-rdbs-mysql56" --container-service-count 17
-_container_check --image-name "local/df/wb/centos/7/rdbs/postresql95" --container-name "df-wb-centos7-rdbs-pgsql95" --container-service-count 19
+_container_check --image-name "local/df/wb/centos/7" --container-name "df-wb-centos7" --container-service-count 18 && \
+_container_check --image-name "local/df/wb/centos/7/httpd/apache" --container-name "df-wb-centos7-httpd" --container-service-count 19 && \
+_container_check --image-name "local/df/wb/centos/7/httpd/apache/php56" --container-name "df-wb-centos7-httpd-php56" --container-service-count 20 && \
+_container_check --image-name "local/df/wb/centos/7/httpd/apache/php70" --container-name "df-wb-centos7-httpd-php70" --container-service-count 20 && \
+_container_check --image-name "local/df/wb/centos/7/rdbs/mysql57" --container-name "df-wb-centos7-rdbs-mysql57" --container-service-count 19 && \
+_container_check --image-name "local/df/wb/centos/7/rdbs/mysql56" --container-name "df-wb-centos7-rdbs-mysql56" --container-service-count 17 && \
+_container_check --image-name "local/df/wb/centos/7/rdbs/postresql95" --container-name "df-wb-centos7-rdbs-pgsql95" --container-service-count 19 && \
 _container_check --image-name "local/df/wb/centos/7/rdbs/postresql93" --container-name "df-wb-centos7-rdbs-pgsql93" --container-service-count 19
