5c23f2b7d485d06a672e4b5c691918962a70bd4a
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2016-07-30 07:50:08 +0200

    fix minor issues on travis processor script

--- df-wb-build/travis.sh
+++ df-wb-build/travis.sh
@@ -6,15 +6,15 @@
 #
 # @copyright (c) 2016 Patrick Paechnatz <patrick.paechnatz@gmail.com>
 # @author patrick.paechnatz@gmail.com
-# @updated 2016/29/07
-# @version 1.0.1
+# @updated 2016/30/07
+# @version 1.0.2
 #
 
 # activate strict mode for bash
 set -e
 
 # some default definitions
-VERSION="1.0.1"
+VERSION="1.0.2"
 
 # some default configuration constants
 SCRIPT_PATH=$(dirname "$0")
@@ -30,15 +30,13 @@ _image_destroy() {
 
     case "${1}" in
         img|--image-name)
-
             img_name="${2}"
-            docker
-            docker rmi -f ${img_name}
-
             shift
         ;;
 
     esac
+
+    docker rmi -f ${img_name}
 }
 
 _container_check() {
@@ -89,10 +87,12 @@ _container_check() {
     echo "wait 3 seconds before service check ..."; sleep 3s
    _cnt=$(docker logs ${cnt_name} | grep '\[*OK' | wc -l)
     if [ $_cnt -eq ${service_count} ]; then
-        echo -e "\033[1;92m[SUCCESS] all required systemd services loaded -> ${img_name} <${cnt_name}>\033[0m";
+        echo -e "\033[1;92m[SUCCESS] all required systemd services loaded -> ${img_name} <${cnt_name}>\033[0m"
+        docker logs ${cnt_name}
     else
         echo -e "\n\033[0;31m[FAILURE] NOT all service loaded successfully! -> ${img_name} <${cnt_name}>\033[0m <EXIT>\n"
-        echo -e "          -> looking for ${service_count} services but found ${_cnt}\n" && docker logs ${cnt_name}
+        echo -e "          -> looking for ${service_count} services but found ${_cnt}\n"
+        docker logs ${cnt_name}
        _docker_container_clear
         exit 9
     fi
@@ -102,11 +102,11 @@ _container_check() {
 }
 
 _docker_container_clear() {
-    echo -e "\n\033[1;92mremove all container \033[0m";
+    echo -e "\n\033[1;92mremove all running container \033[0m";
     docker rm -fv $(docker ps -a -q)
 }
 
-_docker_image_clear() {
+_docker_dangling_image_clear() {
     echo -e "\n\033[1;92mremove all dangling images \033[0m";
     docker images -q --filter "dangling=true" | xargs docker rmi -f
 }
@@ -119,10 +119,10 @@ _show_title
 
 cd ..
 
-echo -e "\n - build all images necessary for this workbench ... \n"
+echo -e "\n [ build all images necessary for this workbench ... ]\n"
 echo "y" | ./dctl --build-all-images
 
-echo -e "\n - check runtime availability for all microServices ... \n"
+echo -e "\n [ check runtime availability for all microServices ... ]\n"
 _container_check --image-name "local/df/wb/centos/7" --container-name "df-wb-centos7" --container-service-count 17 && \
 _container_check --image-name "local/df/wb/centos/7/httpd/apache" --container-name "df-wb-centos7-httpd" --container-service-count 18 && \
 _container_check --image-name "local/df/wb/centos/7/httpd/apache/php56" --container-name "df-wb-centos7-httpd-php56" --container-service-count 19 && \
